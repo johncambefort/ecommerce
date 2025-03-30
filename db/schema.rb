29553d401da7074a0c8622e120073021aa332ca8
John Cambefort <john.cambefort@gmail.com>
2025-03-30 15:46:09 -0400

    feat: create Carts

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_30_190527) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_30_193106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
