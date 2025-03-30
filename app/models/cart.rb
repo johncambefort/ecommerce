a92a9669583224de984aa6fb80e616109e55e4f2
John Cambefort <john.cambefort@gmail.com>
2025-03-30 16:28:18 -0400

    feat: add cart_products

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Cart < ApplicationRecord
+  has_many :cart_products
+  has_many :products, through: :cart_products
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_30_193106) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_30_202157) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "cart_products", force: :cascade do |t|
+    t.bigint "cart_id"
+    t.bigint "product_id"
+    t.integer "quantity", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_cart_products_on_cart_id"
+    t.index ["product_id"], name: "index_cart_products_on_product_id"
+  end
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
